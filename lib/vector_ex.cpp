cce562b6b3f894836e11b0e299113e63fa7d0fb9
Tatsuya Kitabatake <e2tod2@gmail.com>
2016-06-11 14:31:14 +0900

    make libs.

---
+++ lib/vector_ex.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+template<typename T> void debug_vector (vector<T> v) {
+  for (int i = 0, s = v.size(); i < s; i++) {
+    cout << v[i];
+    if (i < s - 1) cout << "\t"; else cout << endl;
+  }
+}
+
+template<typename T> void debug_2vector (vector< vector<T> > v) {
+  for (int i = 0, s = v.size(); i < s; i++) {
+    debug_vector(v[i]);
+    cout << "\n";
+  }
+}
+
+
+template<typename T> vector<T> operator+(const vector<T>& base, const vector<T>& add) {
+  vector<T> ret = base;
+  ret.insert(ret.end(), add.begin(), add.end());
+  return ret;
+}
+
+
+int main (){
+
+  vector<int> base {1 , 4};
+  vector<int> add {3, 5};
+  debug_vector(base + add);
+}
+
+
+
