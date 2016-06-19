80e9efd4ab23cf54245ff5c4343ebec0b86d4964
Tatsuya Kitabatake <e2tod2@gmail.com>
2016-06-19 11:26:29 +0900

    implement lib/union_find.

---
+++ lib/union_find.cpp
@@ -0,0 +1,57 @@
+using namespace std;
+#include <vector>
+
+template<typename T>
+class UnionFind {
+
+  public:
+    
+    UnionFind (int node_num)
+    {
+      nodes = vector<Node>(node_num);
+      for (int i = 0; i < node_num; i++) nodes[i] = {i, 1, -1};
+    }
+
+    void connect (int a_i, int b_i)
+    {
+      Node &a_head = nodes[head_index(a_i)];
+      Node &b_head = nodes[head_index(b_i)];
+
+      if (a_head.connected > b_head.connected) {
+        a_head.connected += b_head.connected;
+        b_head.forward_i = a_head.index;
+      }
+      else {
+        b_head.connected += a_head.connected;
+        a_head.forward_i = b_head.index;
+      }
+    }
+
+    T find (int i)
+    {
+      Node node = nodes[i];
+      while (node.forward_i != -1){
+        node = nodes[node.forward_i];
+      }
+      return node.connected;
+    }
+
+  private:
+    
+    struct Node {
+      int index;
+      T connected;
+      int forward_i;
+    };
+
+    vector<Node> nodes;
+
+    int head_index (int n_i)
+    {
+      Node node = nodes[n_i];
+      while (node.forward_i != -1) {
+        node = nodes[node.forward_i];
+      }
+      return node.index;
+    }
+};
---
+++ lib/union_find_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cassert>
+#include "./union_find.cpp"
+
+int main(){
+  UnionFind<int> uf{5};
+
+  assert(uf.find(0) == 1);
+
+  uf.connect(0, 1);
+  assert(uf.find(0) == 2);
+
+  uf.connect(2, 3);
+  uf.connect(3, 4);
+  assert(uf.find(2) == 3);
+
+  uf.connect(0, 4);
+  assert(uf.find(1) == 5);
+} 
